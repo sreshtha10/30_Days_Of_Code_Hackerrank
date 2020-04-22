b7ed059fda86ab9bed9f0d3a05c8fd126f3dccd7
Sreshtha Mehrotra <55186163+sreshthamehrotra00@users.noreply.github.com>
2020-04-23 00:28:35 +0530

    Create Day_11_2D_Arrays.c

---
+++ Day_11_2D_Arrays.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int arr[6][6],max = -100;
+    for(int i = 0;i<6;i++)
+    {
+        for(int j= 0;j<6;j++)
+        {
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    for(int i = 0;i<4;i++)
+    {
+        int sum;
+        for(int j = 0;j<4;j++)
+        {
+            sum = arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[i+1][j+1]+arr[i+2][j]+arr[i+2][j                    +1]+arr[i+2][j+2];
+            if (sum >max){
+                max = sum;
+            }
+        }
+    }
+    printf("%d",max);
+}
